0b44c491b31bdbc6e839d9ee1bad3b8ecdcad497
Onkar Sonar <30930724+onkarsonar@users.noreply.github.com>
2025-04-21 13:29:50 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwXDPMV_KniX0-0gLUiIfqPZbzFdIDSic",
-  authDomain: "cinemagpt-a2861.firebaseapp.com",
-  projectId: "cinemagpt-a2861",
-  storageBucket: "cinemagpt-a2861.firebasestorage.app",
-  messagingSenderId: "61626451735",
-  appId: "1:61626451735:web:e38feb87706319af8b1952",
-  measurementId: "G-9YBCLJ0CYC"
+  apiKey: "BIzaSZbzFdIDSic",
+  authDomain: "cinemagpt6573.firebaseapp.com",
+  projectId: "cinemagpt-676",
+  storageBucket: "cinemagpt-7764.firebasestorage.app",
+  messagingSenderId: "783883",
+  appId: "1:616239922:web:e38fe673992",
+  measurementId: "K-783bhfeeeeL"
 };
 
 // Initialize Firebase
